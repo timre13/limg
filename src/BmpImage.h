303457034c8a4a2122167f2e6ce4aa28f7982fd1
timre13 <itorteli13@gmail.com>
2021-03-16 15:50:03 +0100

    Add missing header guards.

--- src/BmpImage.h
+++ src/BmpImage.h
@@ -26,6 +26,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#pragma once
+
 #include "Image.h"
 #include <stdint.h>
 #include <vector>
